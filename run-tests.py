59cf56b468d5a2d6f4bf41b492ce0738e9ac8232
FarmHJ <huijia-farm_95@hotmail.com>
2020-12-11 16:32:53 +0800

    Correct tests directory

--- run-tests.py
+++ run-tests.py
@@ -16,7 +16,7 @@ def run_unit_tests():
     """
     Runs unit tests (without subprocesses).
     """
-    tests = os.path.join('seirmo', 'tests')
+    tests = os.path.join('solver', 'tests')
     suite = unittest.defaultTestLoader.discover(tests, pattern='test*.py')
     res = unittest.TextTestRunner(verbosity=2).run(suite)
     sys.exit(0 if res.wasSuccessful() else 1)
