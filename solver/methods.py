bc2a109ec3b7bf94870063b09d0122a244766564
FarmHJ <huijia-farm_95@hotmail.com>
2021-01-18 17:41:23 +0800

    create Euler implicit method

--- solver/methods.py
+++ solver/methods.py
@@ -26,14 +26,43 @@ class SolverMethod(object):
         self.initial_value = float(initial_value)
         self.mesh_points = int(mesh_points)
 
+        self.mesh_size = (self.x_max - self.x_min) / self.mesh_points
+
     def Euler_explicit(self):
 
         y_n = [self.initial_value]
         x_n = [self.x_min]
 
-        mesh_size = (self.x_max - self.x_min) / self.mesh_points
         for n in range(1, self.mesh_points + 1):
-            x_n.append(self.x_min + n * mesh_size)
-            y_n.append(y_n[-1] + mesh_size * self.func(x_n, y_n[-1]))
+            x_n.append(self.x_min + n * self.mesh_size)
+            y_n.append(y_n[-1] + self.mesh_size * self.func(x_n[-1], y_n[-1]))
+
+        return x_n, y_n
+
+    def fixed_pt_iteration(self, init_pred, x):
+        
+        y_0 = init_pred
+        y_1 = init_pred + self.mesh_size * self.func(x, y_0)
+        iteration_counts = 0
+
+        while abs(y_1 - y_0) >= 0.01 and iteration_counts <= 100:
+            y_0 = y_1
+            y_1 = init_pred + self.mesh_size * self.func(x, y_0)
+            iteration_counts += 1
+
+        if abs(y_1 - y_0) < 0.01:
+            return y_1
+        else:
+            raise RuntimeError('Fixed point iteration does not converge.')
+
+    def Euler_implicit(self):
+
+        y_n = [self.initial_value]
+        x_n = [self.x_min]
+
+        for n in range(1, self.mesh_points + 1):
+            x_n.append(self.x_min + n * self.mesh_size)
+            est_y = self.fixed_pt_iteration(y_n[-1], x_n[-1])
+            y_n.append(est_y)
 
         return x_n, y_n
--- solver/tests/test_methods.py
+++ solver/tests/test_methods.py
@@ -45,6 +45,37 @@ class TestSolverMethod(unittest.TestCase):
         self.assertEqual(np.shape(soln), (11,))
         self.assertEqual(soln[1], 0.9)
         self.assertEqual(soln[2], 0.81)
+    
+    def test_fixed_pt_iteration(self):
+
+        func = lambda x, y: -y
+        x_min = 0
+        x_max = 1
+        initial_value = 1
+        mesh_points = 10
+
+        problem = solver.SolverMethod(
+            func, x_min, x_max, initial_value, mesh_points)
+        y_pred = problem.fixed_pt_iteration(initial_value, 0.1)
+
+        self.assertEqual(y_pred, 0.909)
+
+    def test_Euler_implicit(self):
+
+        func = lambda x, y: -y
+        x_min = 0
+        x_max = 1
+        initial_value = 1
+        mesh_points = 10
+
+        problem = solver.SolverMethod(
+            func, x_min, x_max, initial_value, mesh_points)
+        mesh, soln = problem.Euler_implicit()
+
+        self.assertEqual(np.shape(mesh), (11,))
+        self.assertEqual(np.shape(soln), (11,))
+        self.assertEqual(soln[1], 0.909)
+        self.assertAlmostEqual(soln[2], 0.82719)
 
 
 if __name__ == '__main__':
